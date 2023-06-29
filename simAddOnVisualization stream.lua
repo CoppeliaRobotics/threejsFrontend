82929e920b39f71b66411517ec16689daa9ba268
Federico Ferri <federico.ferri.it@gmail.com>
2023-06-29 15:01:19 +0200

    zmq backend

--- simAddOnVisualization stream.lua
+++ simAddOnVisualization stream.lua
@@ -10,7 +10,6 @@ function sysCall_info()
 end
 
 function sysCall_init()
-    simWS=require'simWS'
     cbor=require'org.conman.cbor'
     base64=require'base64'
     url=require'socket.url'
@@ -18,29 +17,64 @@ function sysCall_init()
     sentGenesis={}
     resourcesDir=sim.getStringParameter(sim.stringparam_resourcesdir)
 
-    wsPort=sim.getNamedInt32Param(P'ws.port') or 23020
-    sim.addLog(sim.verbosity_scriptinfos,'WS endpoint on port '..tostring(wsPort)..'...')
-    if sim.getNamedBoolParam(P'ws.retryOnStartFailure') then
-        while true do
-            local r,e=pcall(function() wsServer=simWS.start(wsPort) end)
-            if r then break end
-            sim.addLog(sim.verbosity_scriptwarnings,'WS failed to start ('..e..'). Retrying...')
-            sim.wait(0.5,false)
-        end
-    else
-        wsServer=simWS.start(wsPort)
-    end
-    simWS.setOpenHandler(wsServer,'onWSOpen')
-    simWS.setCloseHandler(wsServer,'onWSClose')
-    simWS.setMessageHandler(wsServer,'onWSMessage')
-    simWS.setHTTPHandler(wsServer,'onWSHTTP')
-    wsClients={}
+    zmqEnable=sim.getNamedBoolParam(P'zmq.enable')
+    wsEnable=sim.getNamedBoolParam(P'ws.enable')
+    if wsEnable==nil then wsEnable=true end
 
-    sim.addLog(sim.verbosity_scriptinfos,'e.g. in your local web browser, type: http://127.0.0.1:'..tostring(wsPort))
+    if zmqEnable then
+        simZMQ=require'simZMQ'
+        simZMQ.__raiseErrors(true) -- so we don't need to check retval with every call
+        zmqPUBPort=sim.getNamedInt32Param(P'zmq.pub.port') or 23010
+        zmqREPPort=sim.getNamedInt32Param(P'zmq.rep.port') or (zmqPUBPort+1)
+        print('ZMQ endpoint on ports '..tostring(zmqPUBPort)..', '..tostring(zmqREPPort)..'...')
+        zmqContext=simZMQ.ctx_new()
+        zmqPUBSocket=simZMQ.socket(zmqContext,simZMQ.PUB)
+        simZMQ.bind(zmqPUBSocket,string.format('tcp://*:%d',zmqPUBPort))
+        zmqREPSocket=simZMQ.socket(zmqContext,simZMQ.REP)
+        simZMQ.bind(zmqPUBSocket,string.format('tcp://*:%d',zmqREPPort))
+    end
+
+    if wsEnable then
+        simWS=require'simWS'
+        wsPort=sim.getNamedInt32Param(P'ws.port') or 23020
+        print('WS endpoint on port '..tostring(wsPort)..'...')
+        if sim.getNamedBoolParam(P'ws.retryOnStartFailure') then
+            while true do
+                local r,e=pcall(function() wsServer=simWS.start(wsPort) end)
+                if r then break end
+                print('WS failed to start ('..e..'). Retrying...')
+                sim.wait(0.5,false)
+            end
+        else
+            wsServer=simWS.start(wsPort)
+        end
+        simWS.setOpenHandler(wsServer,'onWSOpen')
+        simWS.setCloseHandler(wsServer,'onWSClose')
+        simWS.setMessageHandler(wsServer,'onWSMessage')
+        simWS.setHTTPHandler(wsServer,'onWSHTTP')
+        wsClients={}
+    end
+
+    if not zmqEnable and not wsEnable then
+        sim.addLog(sim.verbosity_errors,'aborting because no RPC backend selected')
+        return {cmd='cleanup'}
+    end
 end
 
 function sysCall_addOnScriptSuspend()
     return {cmd='cleanup'}
+end
+
+function sysCall_nonSimulation()
+    processZMQRequests()
+end
+
+function sysCall_sensing()
+    processZMQRequests()
+end
+
+function sysCall_suspended()
+    processZMQRequests()
 end
 
 function sysCall_event(data)
@@ -48,6 +82,12 @@ function sysCall_event(data)
 end
 
 function sysCall_cleanup()
+    if zmqPUBSocket or zmqREPSocket then
+        if zmqPUBSocket then simZMQ.close(zmqPUBSocket) end
+        if zmqREPSocket then simZMQ.close(zmqREPSocket) end
+        simZMQ.ctx_term(zmqContext)
+    end
+
     if wsServer then
         simWS.stop(wsServer)
     end
@@ -61,6 +101,24 @@ function getFileContents(path)
         return 200,content
     else
         return 404,nil
+    end
+end
+
+function processZMQRequests()
+    if not zmqREPSocket then return end
+    while true do
+        local rc,revents=simZMQ.poll({zmqREPSocket},{simZMQ.POLLIN},0)
+        if rc<=0 then break end
+        local rc,req=simZMQ.recv(zmqREPSocket,0)
+        req=cbor.decode(req)
+        local resp=''
+        if req.cmd=='getbacklog' then
+            resp=sim.getGenesisEvents()
+        else
+            sim.addLog(sim.verbosity_errors,'unrecognized request')
+            print(req)
+        end
+        simZMQ.send(zmqREPSocket,resp,0)
     end
 end
 
@@ -83,15 +141,14 @@ end
 
 function onWSHTTP(server,connection,resource,data)
     resource=url.unescape(resource)
-    local mainPage='threejsFrontend'
     local status,data=404,nil
-    if resource=='/' or resource=='/'..mainPage..'.html' then
-        status,data=getFileContents(resourcesDir..'/'..mainPage..'.html')
+    if resource=='/' or resource=='/threejsFrontend.html' then
+        status,data=getFileContents(resourcesDir..'/threejsFrontend.html')
         if status==200 then
             data=string.gsub(data,'const wsPort = 23020;','const wsPort = '..wsPort..';')
         end
-    elseif resource=='/'..mainPage..'.js' then
-        status,data=getFileContents(resourcesDir..'/'..mainPage..'.js')
+    elseif resource=='/threejsFrontend.js' then
+        status,data=getFileContents(resourcesDir..'/threejsFrontend.js')
     elseif resource:sub(1,10)=='/3rdparty/' then
         status,data=getFileContents(resourcesDir..resource)
     end
@@ -104,6 +161,9 @@ end
 function sendEventRaw(d,conn)
     if d==nil then return end
 
+    if zmqPUBSocket then
+        simZMQ.send(zmqPUBSocket,d,0)
+    end
     if wsServer then
         for connection,_ in pairs(wsClients) do
             -- broadcast only after genesis
